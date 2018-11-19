dd36ecf77de71fb44c951703135d91e4289ba3a4
Hamish Hall <hachall@hotmail.com>
2018-11-19 16:23:14 +0000

    added reduxed dev tools

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -15,7 +15,7 @@ import CarsIndex from './containers/cars_index';
 import CarsShow from './containers/cars_show';
 import CarsNew from './containers/cars_new';
 
-const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
+const garageName = prompt("What is your garage?") || `reparator-2000`;
 const init_cars = [
   { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
   { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
@@ -24,7 +24,7 @@ const init_cars = [
 ]
 const initialState = {
   garage: garageName,
-  cars: init_cars
+  cars: init_cars,
 };
 
 const reducers = combineReducers({
@@ -32,7 +32,9 @@ const reducers = combineReducers({
   cars: carsReducer
 });
 
-const middlewares = applyMiddleware(reduxPromise, logger);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(reduxPromise, logger));
 
 // render an instance of the component in the DOM
 ReactDOM.render(
