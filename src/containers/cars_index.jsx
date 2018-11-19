fbbc2a4fddee050f78c7d15a65d14f92e1504fd5
Hamish Hall <hachall@hotmail.com>
2018-11-19 16:24:31 +0000

    fetch cars action, reducer and mapped to props

--- src/containers/cars_index.jsx
+++ src/containers/cars_index.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { fetchCars } from '../actions'
 
 class CarsIndex extends Component {
 
+  // componentWillMount() {
+  //   this.props.fetchCars()
+  // }
+
+  renderCars() {
+    return this.props.cars.map((car) => {
+      <div className="car-card">
+        <h3>{car.brand} - {car.model}</h3>
+        <h4>{car.owner} ({car.late})</h4>
+      </div>
+    });
+
+  }
+
   render() {
     return(
-      <div></div>
+      <div>
+        <h1>Welcome to {this.props.garage}</h1>
+        <div className="cars-list">
+          {this.renderCars()}
+        </div>
+      </div>
     )
   }
 }
 
-export default CarsIndex;
+function mapDispatchToProps(dispatch){
+  return bindActionCreators( { fetchCars }, dispatch)
+}
+
+function mapStateToProps(state) {
+  return {
+    cars: state.cars,
+    garage: state.garage
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CarsIndex);
--- src/reducers/cars_reducer.js
+++ src/reducers/cars_reducer.js
@@ -1,7 +1,9 @@
-// import { FETCH_POSTS, FETCH_POST , POST_CREATED} from '../actions';
+import { FETCH_CARS } from '../actions';
 
 export default function(state = [], action) {
   switch (action.type) {
+    case FETCH_CARS:
+      return action.payload;
     default:
       return state;
   }
